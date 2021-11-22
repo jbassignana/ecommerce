a8e4fbe9f3225367d3339cf83fea9bf868bd0725
Juan Manuel Bassignana <juanbassignana@gmail.com>
2021-11-22 09:57:32 -0300

    0.0.1 working on navbar

---
+++ server/config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'ecommerce-next.pnqji.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'ecommerce'),
+        username: env('DATABASE_USERNAME', 'juanmi70'),
+        password: env('DATABASE_PASSWORD', 'iamriven1'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c9b15cf1-9273-4645-bb77-e13a5169fc5b'
+};
